eeb332d2fa6547eb877087ec1b4f87e4ce0a6cb8
bbhanushali1992-cmd <bbhanushali1992@gmail.com>
2026-04-27 17:18:34 +0530

    Add files via upload

---
+++ src/main/java/SimpleJavaProgram/HelloAutomationExample.java
@@ -0,0 +1,9 @@
+package SimpleJavaProgram;
+
+public class HelloAutomationExample {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Automation");
+        System.out.println("Welcome to Java Programming");
+    }
+}
