a93502877907dd5e3e1bb9bdc7ffb03b6e3267ee
bbhanushali1992-cmd <bbhanushali1992@gmail.com>
2026-04-30 14:16:02 +0530

    Add cron expressions message to HelloAutomationExample
    Added a new print statement about cron expressions.

--- src/main/java/SimpleJavaProgram/HelloAutomationExample.java
+++ src/main/java/SimpleJavaProgram/HelloAutomationExample.java
@@ -9,5 +9,6 @@ System.out.println("Mentor is Ganesh Jadhav");
 System.out.println("Java is object Oriented programming language");
 System.out.println("Skill people can use the  programming language");
         System.out.println("I want to add the jenkins file to repositories automation");
+              System.out.println("we are using simple cron expressions");
     }
 }
