47aa3fb130ee2bbad540869a93e64d9b870642d9
bbhanushali1992-cmd <bbhanushali1992@gmail.com>
2026-04-30 14:04:32 +0530

    Add Jenkins file message to HelloAutomationExample

--- src/main/java/SimpleJavaProgram/HelloAutomationExample.java
+++ src/main/java/SimpleJavaProgram/HelloAutomationExample.java
@@ -8,5 +8,6 @@ public class HelloAutomationExample {
 System.out.println("Mentor is Ganesh Jadhav");
 System.out.println("Java is object Oriented programming language");
 System.out.println("Skill people can use the  programming language");
+        System.out.println("I want to add the jenkins file to repositories automation");
     }
 }
